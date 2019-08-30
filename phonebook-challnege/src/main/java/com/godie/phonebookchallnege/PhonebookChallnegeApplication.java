137d5cda070f874261bcd5f19a036e5f2211c37c
Diego G <godie100@gmail.com>
2019-08-30 23:02:43 +0200

    First version of Spring Boot App

---
+++ phonebook-challnege/src/main/java/com/godie/phonebookchallnege/PhonebookChallnegeApplication.java
@@ -0,0 +1,13 @@
+package com.godie.phonebookchallnege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookChallnegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonebookChallnegeApplication.class, args);
+	}
+
+}
