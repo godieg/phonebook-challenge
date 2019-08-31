8d00023f00ef9d27a095cfeaab606ff4b0fd2ec3
Diego G <godie100@gmail.com>
2019-08-31 11:48:43 +0200

    JPA configuration

---
+++ phonebook-challenge/src/main/java/com/godie/phonebookchallenge/contact/config/JpaConfig.java
@@ -0,0 +1,7 @@
+package com.godie.phonebookchallenge.contact.config;
+
+/**
+ * Created by Diego Garcia on 8/31/19.
+ */
+public class JpaConfig {
+}
---
+++ phonebook-challenge/src/main/java/com/godie/phonebookchallenge/contact/domain/Contact.java
@@ -0,0 +1,7 @@
+package com.godie.phonebookchallenge.contact.domain;
+
+/**
+ * Created by Diego Garcia on 8/31/19.
+ */
+public class Contact {
+}
